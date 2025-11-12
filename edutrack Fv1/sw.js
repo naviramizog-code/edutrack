b7752cd769f7aeac9cdfdc6cdae25c18118c665c
naviramizog-code <navi.rami.zog@gmail.com>
2025-11-11 23:25:19 -0600

    Add files via upload

---
+++ edutrack Fv1/sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'edutrack-v2.1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request).catch(() => {
+          if (event.request.url.includes('.html')) {
+            return caches.match('./index.html');
+          }
+        });
+      })
+  );
+});
